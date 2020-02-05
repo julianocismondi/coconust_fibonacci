f425cc24e4f8045a7d6c2a79b2572cc11680f379
julianocismondi <55262210+julianocismondi@users.noreply.github.com>
2020-02-05 19:42:21 -0300

    Add files via upload

---
+++ fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, aux, i, cantidad;
+
+            string linea;
+
+            Console.Write("INGRESE CUANTOS NUMEROS DE LA SERIE DE FIBONACCI DESEA VER: ");
+            linea = Console.ReadLine();
+             cantidad = int.Parse(linea);
+             a = 0;
+             b =  1;
+
+             for (i = 0; i < cantidad; i++){
+                 aux = a;
+                 a = b;
+                 b = aux + a;
+                 Console.WriteLine(aux);
+
+             }
+
+
+        }
+    }
+}
